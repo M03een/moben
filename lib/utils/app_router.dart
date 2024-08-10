fcd45c9858dff703add47cbac58df10189a3a363
Aburayas <tazshakas@gmail.com>
2024-08-10 12:41:42 +0300

    add splash view

--- lib/utils/app_router.dart
+++ lib/utils/app_router.dart
@@ -2,17 +2,20 @@ import 'package:get/get_navigation/src/routes/get_route.dart';
 import 'package:moben/view/download_view/download_view.dart';
 import 'package:moben/view/home_view/home_view.dart';
 import 'package:moben/view/play_view/play_view.dart';
+import 'package:moben/view/splash_view/splash%20view.dart';
 
 class AppRouter {
-  static String homeViewPath = '/';
+  static String splashViewPath = '/';
+  static String homeViewPath = '/homeViewPath';
   static String downloadViewPath = '/download';
   static String playViewPath = '/play';
   static List<GetPage<dynamic>> routes = [
+    GetPage(name: splashViewPath, page: () => const SplashView()),
     GetPage(name: homeViewPath, page: () => const HomeView()),
     GetPage(name: downloadViewPath, page: () => const DownloadView()),
     GetPage(
       name: playViewPath,
-      page: () =>  PlayView(),
+      page: () =>  const PlayView(),
     ),
   ];
 }
